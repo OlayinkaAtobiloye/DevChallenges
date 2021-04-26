ee7d1a295e2555fe013d98a1406ccf4d2d3830d6
Olayinka <hiqmatolayinka@gmail.com>
2021-04-26 20:01:51 +0100

    adjusted images and nav menu

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-var NavIcon = document.getElementById('NavIcon');
-var NavBar = document.getElementById('NavBar')
+var NavIcon = document.getElementById("NavIcon");
+var NavBar = document.getElementById("NavBar");
 
-console.log(NavIcon)
-NavIcon.addEventListener('click', function(){NavBar.setAttribute('class', 'NavBar2') 
-document.getElementsByTagName('body')[0].style.overflow = 'hidden'})
+count = 0;
+NavIcon.addEventListener("click", function () {
+  count += 1;
+  if (count % 2 == 0) {
+    NavBar.setAttribute("class", "NavBar");
+    NavIcon.setAttribute("class", "NavIcon");
+  } else {
+    NavBar.setAttribute("class", "NavBar2");
+    NavIcon.setAttribute("class", "NavIcon2");
+  }
+  document.getElementsByTagName("body")[0].style.overflow = "hidden";
+});
