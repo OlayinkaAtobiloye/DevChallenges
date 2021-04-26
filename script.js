69cf6d05dd912589144a33951ccdeb3cf3110c93
Olayinka <hiqmatolayinka@gmail.com>
2021-04-26 21:36:14 +0100

    fixed overflow

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var NavIcon = document.getElementById("NavIcon");
 var NavBar = document.getElementById("NavBar");
+var Header = document.getElementsByTagName('header')[0];
 
 count = 0;
 NavIcon.addEventListener("click", function () {
@@ -7,9 +8,12 @@ NavIcon.addEventListener("click", function () {
   if (count % 2 == 0) {
     NavBar.setAttribute("class", "NavBar");
     NavIcon.setAttribute("class", "NavIcon");
+
+    document.getElementsByTagName("body")[0].setAttribute("class", "");
   } else {
     NavBar.setAttribute("class", "NavBar2");
     NavIcon.setAttribute("class", "NavIcon2");
+    document.getElementsByTagName("body")[0].setAttribute("class", "close");
   }
-  document.getElementsByTagName("body")[0].style.overflow = "hidden";
+  
 });
